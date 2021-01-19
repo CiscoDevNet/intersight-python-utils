48daa2448face2134a0eddeda6af7bc68f5794bd
“dsoper2” <dsoper@cisco.com>
2021-01-19 17:23:12 -0600

    PSU example added

--- time_series_example.py
+++ time_series_example.py
@@ -68,6 +68,39 @@ def get_time_series(api_client):
     )
     logger.info(pformat(api_response))
 
+    ##########################
+    logger.info("Query 'PSU stat' time series")
+    req = intersight.model.telemetry_druid_time_series_request.TelemetryDruidTimeSeriesRequest(
+        aggregations=[
+            intersight.model.telemetry_druid_aggregator.TelemetryDruidAggregator(
+                field_name="sumEnergyConsumed",
+                type="doubleSum",
+                name="energyConsumed",
+                field_names=["sumEnergyConsumed"]
+            ),
+        ],
+        query_type="timeseries",
+        data_source=intersight.model.telemetry_druid_data_source.TelemetryDruidDataSource(
+            type="table",
+            name="psu_stat",
+        ),
+        intervals=[
+            "2021-01-01T00:00:00.000Z/2021-01-15T00:00:00.000Z",
+        ],
+        granularity=intersight.model.telemetry_druid_period_granularity.TelemetryDruidPeriodGranularity(
+            type="period",
+            period="P1D",
+        ),
+        context=intersight.model.telemetry_druid_query_context.TelemetryDruidQueryContext(
+            timeout=30,
+            query_id="psu_stat-QueryIdentifier",
+        ),
+    )
+    api_response = api_instance.query_telemetry_time_series(
+        telemetry_druid_time_series_request=req,
+    )
+    logger.info(pformat(api_response))
+
 
 def main():
     # Configure API key settings for authentication
