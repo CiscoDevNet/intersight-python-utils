4b1647071cc243bcbcafdacffc932bea2228ef81
Brandon Beck <beckbran@gmail.com>
2021-01-20 09:18:58 -0600

    added helper docs

--- helpers.py
+++ helpers.py
@@ -1,12 +1,24 @@
+"""Intersight Formatting Helpers
+
+This script provides formatting helper functions for use with the intersight python sdk
+
+The following pip packages are required:
+
+    - tabulate
+
+"""
+
 try:
     from tabulate import tabulate
 except ImportError:
     print("tabulate is required to run this script.\nInstall using the following: `pip install tabulate`")
 
+# Takes date object and returns formatted date string for use with intersight queries
 def format_time(dt):
     s = dt.strftime('%Y-%m-%dT%H:%M:%S.%f')
     return f"{s[:-3]}Z"
 
+# Takes an array of results and creates a formatted table output (assumes all entries have the same keys)
 def print_results_to_table(obj, ignored_fields=[]):
     headers = []
     if 'intersight' in str(type(obj[0])):
