6d6924693db17e82dfdfbc9240976b53f7b39559
pnithyan <pnithyan@cisco.com>
2023-05-11 19:58:49 +0530

    CSCwe91663 -  updated command to get os version

--- os-discovery-tool/oracle-os-version.sh
+++ os-discovery-tool/oracle-os-version.sh
@@ -5,7 +5,7 @@ oracle_version=$(cat /etc/*-release | grep ^VERSION | head -n1 | awk -F"=" '{pri
 
 if [[ $kernel == *"uek"* ]];
   then
-  echo "Oracle Linux (UEK v"$version")"
+  echo "Oracle Linux (UEK "$version")"
 else
   echo $oracle_version
 fi
