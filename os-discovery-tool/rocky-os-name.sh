7ecddb523b017b1b03d96e648bd34a5923a12705
pnithyan <pnithyan@cisco.com>
2023-05-18 13:50:43 +0530

    CSCwe98725 - To get os name and version from /etc/rocky-release file

--- os-discovery-tool/rocky-os-name.sh
+++ os-discovery-tool/rocky-os-name.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cat /etc/centos-release 2>/dev/null | awk '{print $1" "$2" "$4}' | xargs
+FILE=/etc/rocky-release
+if [ -f "$FILE" ]; then
+    # from Rocky Linux 9.1 file name changed to rocky-release
+    cat $FILE 2>/dev/null | awk '{print $1" "$2" "$4}' | xargs
+else
+    cat /etc/centos-release 2>/dev/null | awk '{print $1" "$2" "$4}' | xargs
+fi
