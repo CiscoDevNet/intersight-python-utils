bf23a94fcd6129a23fb34067851e1acdff1db6b9
pnithyan <pnithyan@cisco.com>
2023-08-22 14:40:24 +0530

    CSCwd92285: Ubuntu 22.04 does not contain LTS in version

--- os-discovery-tool/debian-os-version.sh
+++ os-discovery-tool/debian-os-version.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cat /etc/*-release | grep 'VERSION\=' | head -n1 | awk -F"=" '{print $2}' | xargs | \
-awk '{print "Ubuntu Server "$1" "$2}'; 
+cat /etc/*-release | grep 'PRETTY_NAME\=' | head -n1 | awk -F'=' '{print $2}' | awk -F'"' '{print $2}' | awk -F" " '{print $1" Server "$2" "$3}'
