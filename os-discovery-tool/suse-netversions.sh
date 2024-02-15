c4cb0a1ca43428abd426afc36a2e7f2a8c74c238
pnithyan <pnithyan@cisco.com>
2024-02-15 12:40:12 +0530

    CSCwi95330: added curly brackets for variables

--- os-discovery-tool/suse-netversions.sh
+++ os-discovery-tool/suse-netversions.sh
@@ -3,12 +3,12 @@ export PATH=$PATH:/sbin:/usr/sbin
 hwinfocmd=`which hwinfo`
 modinfocmd=`which modinfo`
 drivers=`${hwinfocmd} --network | grep Driver: | awk '{gsub(/"/,"",$2); print $2}'`
-for driver in $drivers; do
-    versionstring=`$modinfocmd $driver | grep ^version:`
-    if [ -z "$versionstring" ]
+for driver in ${drivers}; do
+    versionstring=`${modinfocmd} ${driver} | grep ^version:`
+    if [ -z "${versionstring}" ]
     then
-        $modinfocmd $driver | grep ^vermagic: | awk '{print $2}'
+        ${modinfocmd} ${driver} | grep ^vermagic: | awk '{print $2}'
     else
-        $modinfocmd $driver | grep ^version: | awk -F":[[:space:]]+" '{print $2}'
+        ${modinfocmd} ${driver} | grep ^version: | awk -F":[[:space:]]+" '{print $2}'
     fi
 done
