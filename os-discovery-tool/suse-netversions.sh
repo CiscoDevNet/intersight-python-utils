271e031a252090feb1ba92b30f6de6cbd494374e
pnithyan <pnithyan@cisco.com>
2024-02-15 12:22:29 +0530

    CSCwi95330: fix for multiple adaptors with version or vermagic

--- os-discovery-tool/suse-netversions.sh
+++ os-discovery-tool/suse-netversions.sh
@@ -2,10 +2,13 @@
 export PATH=$PATH:/sbin:/usr/sbin
 hwinfocmd=`which hwinfo`
 modinfocmd=`which modinfo`
-versionstring=`${hwinfocmd} --network | grep Driver: | awk '{print $2}' | xargs ${modinfocmd} | grep ^version:`
-if [ -z "$versionstring" ]
-then
-    ${hwinfocmd} --network | grep Driver: | awk '{print $2}' | xargs ${modinfocmd} | grep ^vermagic: | awk '{print $2}'
-else
-    ${versionstring} | awk -F":[[:space:]]+" '{print $2}'
-fi
+drivers=`${hwinfocmd} --network | grep Driver: | awk '{gsub(/"/,"",$2); print $2}'`
+for driver in $drivers; do
+    versionstring=`$modinfocmd $driver | grep ^version:`
+    if [ -z "$versionstring" ]
+    then
+        $modinfocmd $driver | grep ^vermagic: | awk '{print $2}'
+    else
+        $modinfocmd $driver | grep ^version: | awk -F":[[:space:]]+" '{print $2}'
+    fi
+done
