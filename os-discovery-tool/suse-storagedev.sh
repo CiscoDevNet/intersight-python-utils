a8f6c09c0066be63553c10b31a90fb7982c5ab65
pnithyan <pnithyan@cisco.com>
2024-10-08 12:07:51 +0530

    INTERSIGHT-33113: use name field for nvme when description not available

--- os-discovery-tool/suse-storagedev.sh
+++ os-discovery-tool/suse-storagedev.sh
@@ -2,4 +2,4 @@
 export PATH=$PATH:/sbin:/usr/sbin
 hwinfocmd=`which hwinfo`
 modinfocmd=`which modinfo`
-${hwinfocmd} --storage | grep Driver: | awk '{print $2}' | xargs ${modinfocmd} | grep ^description: | awk -F":[[:space:]]+" '{print $2}'
+${hwinfocmd} --storage | grep Driver: | awk '{print $2}' | xargs ${modinfocmd} | grep -E '(^description:|^name:)'  | awk -F":[[:space:]]+" '{print $2}'
