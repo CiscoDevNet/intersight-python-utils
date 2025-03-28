544f7892b95122596b5355343eb7e386bfe3c63d
pnithyan <pnithyan@cisco.com>
2025-03-28 11:02:36 +0530

    unable to obtain storage driver name due to key name change

--- os-discovery-tool/storagedriver.sh
+++ os-discovery-tool/storagedriver.sh
@@ -4,5 +4,5 @@ lshwcmd=`which lshw`
 lspcicmd=`which lspci`
 for pciaddress in $(${lshwcmd} -C Storage 2>/dev/null | grep "pci@" | awk -F":" '{print $3":"$4}');
 do
-    ${lspcicmd} -v -s ${pciaddress} | grep "Kernel driver" | awk -F":" '{print $2}' | xargs;
+    ${lspcicmd} -v -s ${pciaddress} | grep -E 'Kernel (driver|modules)' | awk -F":" '{print $2}' | xargs;
 done
