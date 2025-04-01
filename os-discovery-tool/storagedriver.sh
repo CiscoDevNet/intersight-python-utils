81e8d3522b14f16fa1c36f5dd3db581be2ba9cf2
pnithyan <pnithyan@cisco.com>
2025-04-01 12:33:45 +0530

    storage driver name - handled the case when kernel_driver and kernel modules are present

--- os-discovery-tool/storagedriver.sh
+++ os-discovery-tool/storagedriver.sh
@@ -4,5 +4,12 @@ lshwcmd=`which lshw`
 lspcicmd=`which lspci`
 for pciaddress in $(${lshwcmd} -C Storage 2>/dev/null | grep "pci@" | awk -F":" '{print $3":"$4}');
 do
-    ${lspcicmd} -v -s ${pciaddress} | grep -E 'Kernel (driver|modules)' | awk -F":" '{print $2}' | xargs;
+    pcioutput=$("$lspcicmd" -v -s "$pciaddress")
+    kernel_driver=$(echo "$pcioutput" | grep "Kernel driver" | awk '{print $NF}')
+    kernel_modules=$(echo "$pcioutput" | grep "Kernel modules" | awk '{print $NF}')
+    if [ -n "$kernel_driver" ]; then
+	    echo $kernel_driver
+    else
+	    echo $kernel_modules
+    fi
 done
