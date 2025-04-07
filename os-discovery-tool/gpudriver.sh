e2f95237684316ebc30113c80a2de8a004aedc5b
pnithyan <pnithyan@cisco.com>
2025-04-07 11:44:35 +0530

    handled error msg change of nvidia gpu

--- os-discovery-tool/gpudriver.sh
+++ os-discovery-tool/gpudriver.sh
@@ -54,10 +54,11 @@ for pciaddress in $(${lshwcmd} -C Display 2>/dev/null | grep "pci@" | awk -F":" 
                   fi
 
               else
-                if [[ (${osvendorlist[*]} =~ ${osvendor}) ]] && [[ ($nvidiavgpu == "Not supported devices in vGPU mode")]];
+                pattern="^No[t]? supported devices in vGPU mode$"
+                if [[ (${osvendorlist[*]} =~ ${osvendor}) ]] && echo "$nvidiavgpu" | grep -Eq "$pattern";
                     then
                     echo "${driver}(compute), ${nvidiagpuversion}"
-                elif [[ ($osvendor == "rhel") || ($osvendor == "red hat") ]] && [[ !($nvidiavgpu == "Not supported devices in vGPU mode") ]];
+                elif [[ ($osvendor == "rhel") || ($osvendor == "red hat") ]] && ! (echo "$nvidiavgpu" | grep -Eq "$pattern");
                     then
                     echo "${driver}(graphics), ${nvidiagpuversion}"
                  fi
